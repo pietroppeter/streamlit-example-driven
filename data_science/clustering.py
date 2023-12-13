2beb730325e837aebaa304cc8453a6dc2d020f46
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-12-13 11:39:19 +0100

    add data science

---
+++ data_science/clustering.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+title = "🖼️ Clustering colors"
+
+def app():
+    st.title(title)
+
+
+if __name__ == "__main__":
+    app()
---
+++ data_science/exploration.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+title = "📊 Data Exploration"
+
+def app():
+    st.title(title)
+
+
+if __name__ == "__main__":
+    app()
